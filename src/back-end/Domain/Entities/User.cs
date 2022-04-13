33c84f2f0cbada38c27aa7f922b78f5c82bab5a7
Denys Liienko <lienko.d.k@gmail.com>
2022-04-13 15:16:59 +0300

    Revert "Add domain classes"

--- src/back-end/Domain/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Entities;
-
-public class User : IEntity
-{
-    public string? Id { get; set; }
-    public string? UserName { get; set; }
-    public string? Email { get; set; }
-    public IEnumerable<UserInProject>? Projects { get; set; }
-}
