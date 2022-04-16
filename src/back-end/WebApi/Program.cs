6b7c82a60f447c9e32c5779676606b85dcd56051
Denys Liienko <lienko.d.k@gmail.com>
2022-04-16 18:58:32 +0300

    add-installers

--- src/back-end/WebApi/Program.cs
+++ src/back-end/WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.ServiceExtension;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext(builder.Configuration, builder.Environment);
+builder.Services.AddRepositories();
 
 var app = builder.Build();
 
